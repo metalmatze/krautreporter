07433d34b9f43a96dd85a8c4d4132782c5a99bb5
MetalMatze <me@metalmatze.de>
2014-11-03 20:18:06 +0100

    Use RecyclerViewAdapter in MainActivity for articles

--- app/src/main/java/de/metalmatze/krautreporter/MainActivity.java
+++ app/src/main/java/de/metalmatze/krautreporter/MainActivity.java
@@ -2,13 +2,38 @@ package de.metalmatze.krautreporter;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import de.metalmatze.krautreporter.adapters.RecyclerAdapter;
+import de.metalmatze.krautreporter.entities.Article;
 
 public class MainActivity extends ActionBarActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        layoutManager.scrollToPosition(0);
+        recyclerView.setLayoutManager(layoutManager);
+
+        ArrayList<Article> articles = new ArrayList<Article>();
+        RecyclerAdapter adapter = new RecyclerAdapter(articles);
+
+        for (int i = 1; i <= 150; i++) {
+            articles.add(new Article(i + ".10.2014", "Artikel Nummer " + i));
+        }
+
+        recyclerView.setAdapter(adapter);
     }
 
 }
