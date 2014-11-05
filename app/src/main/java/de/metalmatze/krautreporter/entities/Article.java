bf80edaa1bda7db928a997ade316cbaae6e2c42a
MetalMatze <me@metalmatze.de>
2014-11-05 01:53:30 +0100

    Article should have uuid, title, date, link and content

--- app/src/main/java/de/metalmatze/krautreporter/entities/Article.java
+++ app/src/main/java/de/metalmatze/krautreporter/entities/Article.java
@@ -1,20 +1,75 @@
 package de.metalmatze.krautreporter.entities;
 
+import java.net.URL;
+import java.util.GregorianCalendar;
+
 public class Article {
 
-    private final String date;
-    private final String headline;
+    private String uuid;
+    private String title;
+    private GregorianCalendar date;
+    private URL link;
+    private String content;
 
-    public Article(String date, String headline) {
+    public Article() {
+    }
+
+    public Article(String uuid, String title, GregorianCalendar date, URL link, String content) {
+        this.uuid = uuid;
+        this.title = title;
         this.date = date;
-        this.headline = headline;
+        this.link = link;
+        this.content = content;
     }
 
-    public String getDate() {
-        return this.date;
+    public String getUuid() {
+        return uuid;
     }
 
-    public String getHeadline() {
-        return this.headline;
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public GregorianCalendar getDate() {
+        return date;
+    }
+
+    public void setDate(GregorianCalendar date) {
+        this.date = date;
+    }
+
+    public URL getLink() {
+        return link;
+    }
+
+    public void setLink(URL link) {
+        this.link = link;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "uuid='" + uuid + '\'' +
+                ", title='" + title + '\'' +
+                ", date=" + date +
+                ", link=" + link +
+                ", content='" + content + '\'' +
+                '}';
     }
 }
