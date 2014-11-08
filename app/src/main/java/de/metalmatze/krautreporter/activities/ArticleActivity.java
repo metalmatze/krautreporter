04606c41301eab1420ca6795109242c0d9054fad
MetalMatze <me@metalmatze.de>
2014-11-08 14:11:44 +0100

    Add back arrow to articleActivity ActionBar

--- app/src/main/java/de/metalmatze/krautreporter/activities/ArticleActivity.java
+++ app/src/main/java/de/metalmatze/krautreporter/activities/ArticleActivity.java
@@ -1,16 +1,18 @@
 package de.metalmatze.krautreporter.activities;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 
 import de.metalmatze.krautreporter.R;
 
-public class ArticleActivity extends Activity {
+public class ArticleActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_article);
+
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
