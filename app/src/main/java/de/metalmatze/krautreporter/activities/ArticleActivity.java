1dc686a01f6cfef23c03fd8f3388c535c748d940
MetalMatze <me@metalmatze.de>
2014-11-21 13:34:44 +0100

    Set contents in ArticleActivity

--- app/src/main/java/de/metalmatze/krautreporter/activities/ArticleActivity.java
+++ app/src/main/java/de/metalmatze/krautreporter/activities/ArticleActivity.java
@@ -2,8 +2,16 @@ package de.metalmatze.krautreporter.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.activeandroid.query.Select;
+
+import java.text.SimpleDateFormat;
 
 import de.metalmatze.krautreporter.R;
+import de.metalmatze.krautreporter.models.ArticleModel;
 
 public class ArticleActivity extends ActionBarActivity {
 
@@ -11,8 +19,21 @@ public class ArticleActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_article);
-
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String uuid = this.getIntent().getStringExtra("uuid");
+        Log.d("uuid in article activity", uuid);
+        ArticleModel model = new Select().from(ArticleModel.class).where("uuid = ?", uuid).executeSingle();
+
+        TextView articleTitle = (TextView) findViewById(R.id.article_title);
+        TextView articleDate = (TextView) findViewById(R.id.article_date);
+        ImageView articleImage = (ImageView) findViewById(R.id.image);
+        TextView articleExcerpt = (TextView) findViewById(R.id.article_excerpt);
+        TextView articleContent = (TextView) findViewById(R.id.article_content);
+
+        articleTitle.setText(model.title);
+        articleDate.setText(new SimpleDateFormat("dd.MM.yyyy").format(model.date.getTime()));
+        articleContent.setText(model.content);
     }
 
 }
