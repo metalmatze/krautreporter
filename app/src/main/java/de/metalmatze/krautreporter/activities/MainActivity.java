f2edd9fa64699b58fdd4c53908eb17516ea7415c
MetalMatze <me@metalmatze.de>
2014-11-21 13:36:04 +0100

    Use ArticleModel in MainActivity

--- app/src/main/java/de/metalmatze/krautreporter/activities/MainActivity.java
+++ app/src/main/java/de/metalmatze/krautreporter/activities/MainActivity.java
@@ -4,31 +4,32 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
-import com.newrelic.agent.android.NewRelic;
+import com.activeandroid.query.Select;
 
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.List;
 
 import de.metalmatze.krautreporter.KrautreporterRssParser;
 import de.metalmatze.krautreporter.R;
 import de.metalmatze.krautreporter.adapters.ArticleRecyclerViewAdapter;
-import de.metalmatze.krautreporter.entities.Article;
+import de.metalmatze.krautreporter.models.ArticleModel;
 
 public class MainActivity extends ActionBarActivity {
 
     protected RecyclerView recyclerView;
     protected ArticleRecyclerViewAdapter recyclerViewAdapter;
 
-    private ArrayList<Article> articles;
+    private List<ArticleModel> articles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        NewRelic.withApplicationToken("AAcd87d4b9197cb6e3184ac6d5b78f1f1d42488de6").start(this.getApplication());
+//        NewRelic.withApplicationToken("AAcd87d4b9197cb6e3184ac6d5b78f1f1d42488de6").start(this.getApplication());
 
         this.setContentView(R.layout.activity_main);
 
@@ -39,15 +40,29 @@ public class MainActivity extends ActionBarActivity {
         layoutManager.scrollToPosition(0);
         this.recyclerView.setLayoutManager(layoutManager);
 
-        try {
+        int count = new Select().from(ArticleModel.class).count();
 
-            BufferedInputStream inputStream = new BufferedInputStream(getAssets().open("krautreporter.rss"));
-            this.articles = new KrautreporterRssParser().parse(inputStream);
+        Log.d("article count", String.valueOf(count));
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (XmlPullParserException e) {
-            e.printStackTrace();
+        if (count < 1)
+        {
+            try {
+
+                BufferedInputStream inputStream = new BufferedInputStream(getAssets().open("krautreporter.rss"));
+                this.articles = new KrautreporterRssParser().parse(inputStream);
+
+                for (ArticleModel articleModel: this.articles)
+                {
+                    articleModel.save();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            }
+        } else {
+            this.articles = new Select().from(ArticleModel.class).execute();
         }
 
         this.recyclerViewAdapter = new ArticleRecyclerViewAdapter(this.articles);
