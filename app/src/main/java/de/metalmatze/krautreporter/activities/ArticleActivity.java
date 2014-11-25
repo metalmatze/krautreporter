f8bf22ddb63b6edf3a56c8013780998f69364831
MetalMatze <me@metalmatze.de>
2014-11-25 01:17:56 +0100

    Convert HTML content to spanned to use in content TextView

--- app/src/main/java/de/metalmatze/krautreporter/activities/ArticleActivity.java
+++ app/src/main/java/de/metalmatze/krautreporter/activities/ArticleActivity.java
@@ -2,6 +2,7 @@ package de.metalmatze.krautreporter.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Html;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,7 +22,7 @@ public class ArticleActivity extends ActionBarActivity {
         this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         long id = getIntent().getLongExtra("id", -1);
-        ArticleModel model = new Select().from(ArticleModel.class).where("id = ?", id).executeSingle();
+        ArticleModel articleModel = new Select().from(ArticleModel.class).where("id = ?", id).executeSingle();
 
         TextView articleTitle = (TextView) findViewById(R.id.article_title);
         TextView articleDate = (TextView) findViewById(R.id.article_date);
@@ -29,9 +30,9 @@ public class ArticleActivity extends ActionBarActivity {
         TextView articleExcerpt = (TextView) findViewById(R.id.article_excerpt);
         TextView articleContent = (TextView) findViewById(R.id.article_content);
 
-        articleTitle.setText(model.title);
-        articleDate.setText(new SimpleDateFormat("dd.MM.yyyy").format(model.date.getTime()));
-        articleContent.setText(model.content);
+        articleTitle.setText(articleModel.title);
+        articleDate.setText(new SimpleDateFormat("dd.MM.yyyy").format(articleModel.date.getTime()));
+        articleContent.setText(Html.fromHtml(articleModel.content));
     }
 
 }
