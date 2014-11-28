595b01a9cece9b93a047509f36027665657deaad
MetalMatze <me@metalmatze.de>
2014-11-28 02:11:10 +0100

    Use CardView in ArticleRecyclerViewAdapter

--- app/src/main/java/de/metalmatze/krautreporter/adapters/ArticleRecyclerViewAdapter.java
+++ app/src/main/java/de/metalmatze/krautreporter/adapters/ArticleRecyclerViewAdapter.java
@@ -2,11 +2,18 @@ package de.metalmatze.krautreporter.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
@@ -35,13 +42,38 @@ public class ArticleRecyclerViewAdapter extends RecyclerView.Adapter<ArticleRecy
     }
 
     @Override
-    public void onBindViewHolder(ArticleRecyclerViewAdapter.ViewHolder viewHolder, int position) {
+    public void onBindViewHolder(final ArticleRecyclerViewAdapter.ViewHolder viewHolder, int position) {
 
-	this.article = this.articles.get(position);
+        ArticleModel article = this.articles.get(position);
 
         String articleDate = new SimpleDateFormat("dd.MM.yyyy").format(article.date.getTime());
         viewHolder.article_date.setText(articleDate);
         viewHolder.article_headline.setText(article.title);
+
+        if (article.image != null)
+        {
+            viewHolder.article_image.setVisibility(View.VISIBLE);
+
+            ImageRequest imageRequest = new ImageRequest(
+                    article.image,
+                    new Response.Listener<Bitmap>() {
+                        @Override
+                        public void onResponse(Bitmap bitmap) {
+                            viewHolder.article_image.setImageBitmap(bitmap);
+                        }
+                    },
+                    0,0,null,
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+                            volleyError.printStackTrace();
+                        }
+                    }
+            );
+
+            Volley.newRequestQueue(this.context).add(imageRequest);
+        }
+
     }
 
     @Override
@@ -51,14 +83,18 @@ public class ArticleRecyclerViewAdapter extends RecyclerView.Adapter<ArticleRecy
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-	public TextView article_date;
+        public ImageView article_image;
         public TextView article_headline;
+        public TextView article_date;
+        public TextView article_excerpt;
 
         public ViewHolder(final View itemView) {
             super(itemView);
 
-	    this.article_date = (TextView) itemView.findViewById(R.id.article_date);
+            this.article_image = (ImageView) itemView.findViewById(R.id.article_image);
             this.article_headline = (TextView) itemView.findViewById(R.id.article_title);
+            this.article_date = (TextView) itemView.findViewById(R.id.article_date);
+            this.article_excerpt = (TextView) itemView.findViewById(R.id.article_excerpt);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
