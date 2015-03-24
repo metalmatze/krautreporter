f3dcd0128d16f3d364fd2895194951a7d47b3c17
MetalMatze <me@metalmatze.de>
2015-03-24 23:46:53 +0100

    Create a Article extend RealmObject

--- app/src/main/java/de/metalmatze/krautreporter/models/Article.java
+++ app/src/main/java/de/metalmatze/krautreporter/models/Article.java
@@ -1,30 +1,140 @@
 package de.metalmatze.krautreporter.models;
 
-import java.util.Date;
+import io.realm.RealmObject;
+import io.realm.annotations.Index;
+import io.realm.annotations.PrimaryKey;
 
-public interface Article {
+public class Article extends RealmObject {
 
-    Long getId();
-    Long save();
+    @PrimaryKey
+    private int id;
 
-    String getUuid();
-    void setUuid(String uuid);
+    private int order;
 
-    String getTitle();
-    void setTitle(String title);
+    private String title;
 
-    Date getDate();
-    void setDate(Date date);
+    private String headline;
 
-    String getLink();
-    void setLink(String link);
+    private String date;
 
-    String getImage();
-    void setImage(String image);
+    private boolean morgenpost;
 
-    String getExcerpt();
-    void setExcerpt(String excerpt);
+    @Index
+    private String url;
 
-    String getContent();
-    void setContent(String content);
+    private String image;
+
+    private String excerpt;
+
+    private String content;
+
+    private int author;
+
+    private String created_at;
+
+    private String updated_at;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public boolean isMorgenpost() {
+        return morgenpost;
+    }
+
+    public void setMorgenpost(boolean morgenpost) {
+        this.morgenpost = morgenpost;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getExcerpt() {
+        return excerpt;
+    }
+
+    public void setExcerpt(String excerpt) {
+        this.excerpt = excerpt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(int author) {
+        this.author = author;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
 }
