a9a87f60c2442a6d0a432b388c4d8bb9bed1c887
Matthias Loibl <me@metalmatze.de>
2015-10-25 02:48:26 +0200

    Remove helper methods from Api class

--- app/src/main/java/de/metalmatze/krautreporter/api/Api.java
+++ app/src/main/java/de/metalmatze/krautreporter/api/Api.java
@@ -1,43 +1,25 @@
 package de.metalmatze.krautreporter.api;
 
 import android.content.Context;
-import android.support.annotation.Nullable;
 
 import com.google.gson.ExclusionStrategy;
 import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.util.List;
-
 import de.metalmatze.krautreporter.R;
-import de.metalmatze.krautreporter.models.Article;
-import de.metalmatze.krautreporter.models.Author;
-import de.metalmatze.krautreporter.models.Image;
-import io.realm.Realm;
 import io.realm.RealmObject;
-import retrofit.Callback;
-import retrofit.RestAdapter;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
-import retrofit.converter.GsonConverter;
-import rx.Observable;
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
+import retrofit.RxJavaCallAdapterFactory;
 
 public class Api {
 
-    public interface ApiCallback {
-        public void finished();
-    }
-
     protected final Context context;
-    protected Realm realm;
-
     private static ApiInterface singleton;
 
     public ApiInterface request() {
-
         if (singleton == null) {
-
             Gson gson = new GsonBuilder()
                     .setExclusionStrategies(new ExclusionStrategy() {
                         @Override
@@ -54,12 +36,12 @@ public class Api {
                     .excludeFieldsWithoutExposeAnnotation()
                     .create();
 
-            RestAdapter restAdapter = new RestAdapter.Builder()
-                    .setEndpoint(context.getString(R.string.url_api))
-                    .setConverter(new GsonConverter(gson))
-                    .build();
-
-            singleton = restAdapter.create(ApiInterface.class);
+            singleton = new Retrofit.Builder()
+                    .baseUrl(context.getString(R.string.url_api))
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                    .build()
+                    .create(ApiInterface.class);
         }
 
         return singleton;
@@ -71,117 +53,5 @@ public class Api {
 
     public Api(Context context) {
         this.context = context;
-        this.realm = Realm.getInstance(context);
-    }
-
-    public Observable<JsonArray<Article>> articles() {
-        return singleton.articles();
-    }
-
-    public void updateArticles(@Nullable final ApiCallback apiCallback) {
-        request().articles(new Callback<JsonArray<Article>>() {
-            @Override
-            public void success(JsonArray<Article> jsonArticles, Response response) {
-
-                saveArticles(jsonArticles);
-
-                if (apiCallback != null) {
-                    apiCallback.finished();
-                }
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                error.printStackTrace();
-
-                if (apiCallback != null) {
-                    apiCallback.finished();
-                }
-            }
-        });
-    }
-
-    public void updateArticlesOlderThan(int id, @Nullable final ApiCallback apiCallback) {
-        request().articlesOlderThan(id, new Callback<JsonArray<Article>>() {
-            @Override
-            public void success(JsonArray<Article> articleJsonArray, Response response) {
-
-                saveArticles(articleJsonArray);
-
-                if (apiCallback != null) {
-                    apiCallback.finished();
-                }
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                error.printStackTrace();
-
-                if (apiCallback != null) {
-                    apiCallback.finished();
-                }
-            }
-        });
-    }
-
-    private void saveArticles(JsonArray<Article> jsonArticles) {
-        realm.beginTransaction();
-        List<Article> realmArticles = realm.copyToRealmOrUpdate(jsonArticles.data);
-
-        int articleIndex = 0;
-        for (Article article : realmArticles) {
-            int authorId = jsonArticles.data.get(articleIndex).getSerializedAuthor();
-            Author author = realm.where(Author.class)
-                    .equalTo("id", authorId)
-                    .findFirst();
-            article.setAuthor(author);
-
-            List<Image> serializedImages = jsonArticles.data.get(articleIndex).getSerializedImages().data;
-            List<Image> realmImages = realm.copyToRealmOrUpdate(serializedImages);
-
-            for (Image image : realmImages) {
-                article.getImages().add(image);
-            }
-
-            articleIndex++;
-        }
-
-        realm.commitTransaction();
-    }
-
-    public void updateAuthors(@Nullable final ApiCallback apiCallback) {
-        request().authors(new Callback<JsonArray<Author>>() {
-            @Override
-            public void success(JsonArray<Author> jsonAuthors, Response response) {
-                realm.beginTransaction();
-                List<Author> realmAuthors = realm.copyToRealmOrUpdate(jsonAuthors.data);
-
-                int authorIndex = 0;
-                for (Author author : realmAuthors) {
-                    List<Image> serializedImages = jsonAuthors.data.get(authorIndex).getSerializedImages().data;
-                    List<Image> realmImages = realm.copyToRealmOrUpdate(serializedImages);
-
-                    for (Image image : realmImages) {
-                        author.getImages().add(image);
-                    }
-
-                    authorIndex++;
-                }
-
-                realm.commitTransaction();
-
-                if(apiCallback != null) {
-                    apiCallback.finished();
-                }
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                error.printStackTrace();
-                if(apiCallback != null) {
-                    apiCallback.finished();
-                }
-            }
-        });
     }
 }
