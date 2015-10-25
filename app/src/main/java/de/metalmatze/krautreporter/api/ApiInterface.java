1aea7ca162e76c48fd5738d42b43f4f21bbc365f
Matthias Loibl <me@metalmatze.de>
2015-10-25 02:49:17 +0200

    Simplify the ApiInterface with only Observables

--- app/src/main/java/de/metalmatze/krautreporter/api/ApiInterface.java
+++ app/src/main/java/de/metalmatze/krautreporter/api/ApiInterface.java
@@ -2,30 +2,13 @@ package de.metalmatze.krautreporter.api;
 
 import de.metalmatze.krautreporter.models.Article;
 import de.metalmatze.krautreporter.models.Author;
-import retrofit.Callback;
 import retrofit.http.GET;
-import retrofit.http.Path;
-import retrofit.http.Query;
 import rx.Observable;
 
 public interface ApiInterface {
-
-    @GET("/articles")
-    void articles(Callback<JsonArray<Article>> callback);
-
     @GET("/articles")
     Observable<JsonArray<Article>> articles();
 
-    @GET("/articles")
-    void articlesOlderThan(@Query("olderthan") int id, Callback<JsonArray<Article>> callback);
-
-    @GET("/articles/{id}")
-    void article(@Path("id") int articleId, Callback<JsonObject<Article>> callback);
-
     @GET("/authors")
-    void authors(Callback<JsonArray<Author>> callback);
-
-    @GET("/authors/{id}")
-    void author(@Path("id") int authorId, Callback<JsonObject<Author>> callback);
-
+    Observable<JsonArray<Author>> authors();
 }
