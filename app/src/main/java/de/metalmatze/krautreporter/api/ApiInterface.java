caa685cbff371a6f44cef63845049b65df58dc40
Matthias Loibl <me@metalmatze.de>
2015-10-25 15:46:16 +0100

    Create articlesOlderThan api call

--- app/src/main/java/de/metalmatze/krautreporter/api/ApiInterface.java
+++ app/src/main/java/de/metalmatze/krautreporter/api/ApiInterface.java
@@ -3,11 +3,15 @@ package de.metalmatze.krautreporter.api;
 import de.metalmatze.krautreporter.models.Article;
 import de.metalmatze.krautreporter.models.Author;
 import retrofit.http.GET;
+import retrofit.http.Query;
 import rx.Observable;
 
 public interface ApiInterface {
     @GET("/articles")
     Observable<JsonArray<Article>> articles();
+
+    @GET("/articles")
+    Observable<JsonArray<Article>> articlesOlderThan(@Query("olderthan") int id);
 
     @GET("/authors")
     Observable<JsonArray<Author>> authors();
