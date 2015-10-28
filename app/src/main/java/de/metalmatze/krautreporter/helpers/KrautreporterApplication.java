05b3abe40018a3592dbe7120f97913fd17181b10
Matthias Loibl <me@metalmatze.de>
2015-10-28 21:41:13 +0100

    Instantiate dagger in KrautreporterApplication

--- app/src/main/java/de/metalmatze/krautreporter/helpers/KrautreporterApplication.java
+++ app/src/main/java/de/metalmatze/krautreporter/helpers/KrautreporterApplication.java
@@ -2,18 +2,32 @@ package de.metalmatze.krautreporter.helpers;
 
 import android.app.Application;
 
+import de.metalmatze.krautreporter.DaggerKrautreporterComponent;
+import de.metalmatze.krautreporter.KrautreporterComponent;
+import de.metalmatze.krautreporter.KrautreporterModule;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
 public class KrautreporterApplication extends Application {
+
+    public KrautreporterComponent krautreporterComponent;
+
     @Override
     public void onCreate() {
         super.onCreate();
+
+        this.krautreporterComponent = DaggerKrautreporterComponent.builder()
+                .krautreporterModule(new KrautreporterModule(this))
+                .build();
 
         RealmConfiguration configuration = new RealmConfiguration.Builder(this)
                 .deleteRealmIfMigrationNeeded()
                 .build();
 
-        Realm.getInstance(configuration);
+        Realm.setDefaultConfiguration(configuration);
+    }
+
+    public KrautreporterComponent getKrautreporterComponent() {
+        return this.krautreporterComponent;
     }
 }
